fcdf52a90a6b56f74b124d172c7a76de2b58f044
matheus_cascalho <cascalhom@gmail.com>
2021-09-05 23:22:25 -0300

    Variable and FixedRent dataclasses

--- entities/investments.py
+++ entities/investments.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from dataclasses import dataclass
 from datetime import date
-
+from constants import VariableRentTypes, Sectors
+from investment_attributes import Rent
+from abc import abstractmethod
 
 @dataclass
 class Investment:
@@ -9,4 +11,31 @@ class Investment:
     unitary_value: float
     amount: int
     acquisition: date
+
+    @abstractmethod
+    def current_value(self) -> float:
+        pass
+
+
+@dataclass
+class FixedRent(Investment):
+    rent: Rent
+    liquidity: int
+    due_date: date
+    pay_tax: bool = True
+
+    def current_value(self):
+        ...
+
+
+@dataclass
+class VariableRent(Investment):
+    type: VariableRentTypes
+    company: str
+    sector: Sectors
+    has_dividend: bool = False
+
+    def current_value(self) -> float:
+        ...
+
 
