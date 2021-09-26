d1c3da81c91cebae5863b3a8dd0dba04b4280680
matheus_cascalho <cascalhom@gmail.com>
2021-09-26 18:05:47 -0300

    Enums ordenáveis

--- entities/constants.py
+++ entities/constants.py
@@ -1,17 +1,42 @@
 from enum import Enum
 
 
-class InvestmentTypes(Enum):
+class OrderedEnum(Enum):
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
+
+    def __str__(self):
+        return self.value
+
+
+class InvestmentTypes(OrderedEnum):
     FIXED_RENT = "fixed rent"
     VARIABLE_RENT = "variable rent"
 
 
-class VariableRentTypes(Enum):
+class VariableRentTypes(OrderedEnum):
     STOCK = "stock"
     REAL_STATE_INVESTMENT_FUND = "real state investment fund"
 
 
-class Sectors(Enum):
+class Sectors(OrderedEnum):
     BANKING = "banking"
     MINING = "mining"
     RETAIL = "retail"
@@ -19,20 +44,20 @@ class Sectors(Enum):
     PAPER_FUND = "paper fund"
 
 
-class BaseRates(Enum):
+class BaseRates(OrderedEnum):
     SELIC = "selic"
     CDB = "cdb"
     IPCA = "ipca"
     CDI = "cdi"
 
 
-class Coin(Enum):
+class Coin(OrderedEnum):
     USD = "us"
     BRL = "brl"
 
 
-class StockExchanges(Enum):
+class StockExchanges(OrderedEnum):
     B3 = "b3"
 
-class ExchangeRegion(Enum):
+class ExchangeRegion(OrderedEnum):
     SAO_PAULO = "SAO"
